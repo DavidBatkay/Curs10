2fa669066ffd7ab64711f0a25451b448a636d195
DavidBatkay <david.batkay@emanuel.ro>
2024-05-23 12:38:24 +0300

    update

--- fazan.js
+++ fazan.js
@@ -5,17 +5,29 @@ class Button {
     #w;
     #h;
     #text;
+
+    constructor(x, y, w, h, text, color) {
+        this.#x = x;
+        this.#y = y;
+        this.#w = w;
+        this.#h = h;
+        this.#text = text;
+        this.#color = color;
+    }
     draw() {
         fill(this.#color);
-        textSize(50);
-        text(this.#text, this.#x);
+        rect(this.#x, this.#y, this.#w, this.#h);
+        fill("black")
+        textSize(30);
+        text(this.#text, this.#x + 8, this.#y + 35);
     }
-    click() {
+    click(letter) {
         if (mouseX >= this.#x &&
             mouseX <= this.#x + this.#w &&
             mouseY >= this.#y &&
             mouseY <= this.#y + this.#h) {
-            console.log("button was clicked", this.#text);
+            // console.log("button was clicked", this.#text);
+            letter.nextLetter();
         }
     }
 }
@@ -39,5 +51,16 @@ class Letter {
         fill(this.#color);
         textSize(50);
         text(this.#letter, this.#x, this.#y);
+    }
+
+    startCounting() {
+        setInterval(this.nextLetter, this.#speed, this)
+    }
+
+    nextLetter(letter) {
+        const finalLetter = "z".charCodeAt();
+        const letterCode = letter.charCodeAt() + 1;
+        if (finalLetter = letterCode) { letter = "a" }
+        letter.#letter = String.fromCharCode(letterCode);
     }
 }
--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const buttonStart = new Button(200, 300, 40, 20, "Start", "green")
 const buttonStop = new Button(200, 300, 40, 20, "Stop", "red")
+const letter = new Letter("A", 2, "red", 100, 300);
 
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
+  clear();
   buttonStart.draw();
   buttonStop.draw();
+  letter.draw();
+
 }
 function mouseClicked() {
-  buttonStart.click();
-  buttonStop.click();
+  buttonStart.click(letter);
+  buttonStop.click(letter);
 
 }
